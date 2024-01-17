16f888d9e5881fa768d3124a78ea4a86a1f9e8b3
Ola <Hollabodey.kilaso@gmail.com>
2024-01-17 22:25:28 +0000

    Made changes to the script and css file

--- Design 1/script.js
+++ Design 1/script.js
@@ -3,16 +3,30 @@ let vertical = document.querySelectorAll(".vertical");
 let modal = document.querySelector(".moreModal");
 
 startBtn.addEventListener("click", () => {
-  alert("Working!");
+  //   alert("Working!");
+  function timer() {
+    var sec = 30;
+    
+    var timer = setInterval(function () {
+      document.querySelector(".secs").innerText = ":" + sec;
+      sec--;
+      if (sec < 0) {
+        clearInterval(timer);
+      }
+
+    }, 1000);
+  }
+
 });
 
-
 vertical.forEach((moreVertical) => {
+
   moreVertical.addEventListener("click", () => {
     if (modal.style.display === "none") {
       modal.style.display = "block";
     } else {
       modal.style.display = "none";
     }
+
   });
 });
