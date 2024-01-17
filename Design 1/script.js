f4687c8acb1e7de478955eb02ee6767bfab6dceb
Ola <Hollabodey.kilaso@gmail.com>
2024-01-17 22:28:32 +0000

    Made changes to the index and js file

--- Design 1/script.js
+++ Design 1/script.js
@@ -3,30 +3,34 @@ let vertical = document.querySelectorAll(".vertical");
 let modal = document.querySelector(".moreModal");
 
 startBtn.addEventListener("click", () => {
-  //   alert("Working!");
-  function timer() {
-    var sec = 30;
-    
-    var timer = setInterval(function () {
-      document.querySelector(".secs").innerText = ":" + sec;
-      sec--;
-      if (sec < 0) {
-        clearInterval(timer);
-      }
-
-    }, 1000);
-  }
-
+//   alert("Working!");
+  timer();
 });
 
-vertical.forEach((moreVertical) => {
 
+vertical.forEach((moreVertical) => {
   moreVertical.addEventListener("click", () => {
+    
     if (modal.style.display === "none") {
       modal.style.display = "block";
     } else {
       modal.style.display = "none";
     }
-
   });
 });
+
+
+
+function timer(){
+    var sec = 30;
+    var timer = setInterval(function(){
+
+        document.querySelector('.secs').innerText = ":" + sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+        }
+
+    }, 1000);
+}
+
