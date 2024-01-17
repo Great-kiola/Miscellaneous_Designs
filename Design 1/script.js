7f106fca43814ce7f1068035590113274cf7d345
Ola <Hollabodey.kilaso@gmail.com>
2024-01-17 23:01:56 +0000

    Made changes to the html, css and js file

--- Design 1/script.js
+++ Design 1/script.js
@@ -1,20 +1,34 @@
 let startBtn = document.querySelector("#startBtn");
 let vertical = document.querySelectorAll(".vertical");
 let modal = document.querySelector(".moreModal");
+let stopDiv = document.querySelector(".stopDiv");
+
+var timer;
 
 startBtn.addEventListener("click", () => {
     var sec = 30;
+    stopDiv.style.display = "block";
 
-    var timer = setInterval(function(){
+    timer = setInterval(function(){
         document.querySelector('.secs').innerText = ":" + sec;
         sec--;
-        if (sec < 0) {
+
+        if (sec <= 0) {
             clearInterval(timer);
+        }
+        
+        if (sec <= 9 ){
+            document.querySelector('.secs').innerText = ":0" + sec;
         }
 
     }, 1000);
 });
 
+
+stopDiv.addEventListener("click" , () => {
+    clearInterval(timer);
+    document.querySelector('.secs').innerText = ":00";
+});
 
 vertical.forEach((moreVertical) => {
   moreVertical.addEventListener("click", () => {
