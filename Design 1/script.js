20f085dcd3cf2ecab4d897af0f97f2fee8381036
Ola <Hollabodey.kilaso@gmail.com>
2024-01-17 22:08:57 +0000

    Made changes to the html and css file

--- Design 1/script.js
+++ Design 1/script.js
@@ -1,6 +1,18 @@
 let startBtn = document.querySelector("#startBtn");
-
+let vertical = document.querySelectorAll(".vertical");
+let modal = document.querySelector(".moreModal");
 
 startBtn.addEventListener("click", () => {
-    alert("Working!");
+  alert("Working!");
+});
+
+
+vertical.forEach((moreVertical) => {
+  moreVertical.addEventListener("click", () => {
+    if (modal.style.display === "none") {
+      modal.style.display = "block";
+    } else {
+      modal.style.display = "none";
+    }
+  });
 });
