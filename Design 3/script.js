13c3035564a571d975cb901f6694266e1cb7b998
Ola <Hollabodey.kilaso@gmail.com>
2024-01-22 00:35:14 +0000

    Made changes to the js file

--- Design 3/script.js
+++ Design 3/script.js
@@ -17,3 +17,23 @@ coin.forEach((coins) => {
 // coin.addEventListener("mouseleave", () => {
 //     coinModal.style.display = "none";
 // });
+
+
+
+
+const url = 'https://currency-converter18.p.rapidapi.com/api/v1/convert?from=EUR&to=KWD&amount=10';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0c84b4802amsh669d15d5e5d8739p13d073jsnd008c486b220',
+		'X-RapidAPI-Host': 'currency-converter18.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
